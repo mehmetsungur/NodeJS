aeb691d6d9c84dd6b88c43736c35e822fd340a42
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-16 17:04:00 +0300

    update

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Merhaba NodeJS");
+var http = require("http");
+
+function requestListener(request, response) {
+    response.end();
+}
+
+var server = http.createServer(requestListener);
+
+server.listen(3000);
+
+console.log("node.js server at port 3000");
