cca86ee390deeb3bb8a45191eb1e64433121a899
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-16 17:18:20 +0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var http = require("http");
 
 var server = http.createServer((request, response) => {
-    console.log(request.url, request.method);
-    console.log(response.statusCode);
+    // console.log(request.url, request.method);
+    // console.log(response.statusCode);
+
+    response.setHeader("Content-Type", "text/html");
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+
+    response.write("<h1>Anasayfa</h1>");
+    response.write("<p>Urunler</p>");
+
     response.end();
-})
+});
 
 
 server.listen(3000);
