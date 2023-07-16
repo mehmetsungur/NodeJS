58f9442951a23b25fd2458c2baad2287e10d53a0
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-16 18:12:42 +0300

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer((request, response) => {
+
     if (request.url == '/') {
         fs.readFile("index.html", (error, html) => {
             response.writeHead(200, { "Content-Type": "text/html" });
@@ -9,31 +10,45 @@ var server = http.createServer((request, response) => {
             response.end();
         });
     }
-    else if (request.url == '/blogs') {
+
+    else if (request.url == "/blogs") {
         fs.readFile("blogs.html", (error, html) => {
             response.writeHead(200, { "Content-Type": "text/html" });
             response.write(html);
             response.end();
         });
     }
-    else if (request.url == '/create' && request.method == "POST") {
-        fs.appendFile("blogs.txt", "deneme", (err) => {
-            if (err) {
-                console.log(err)
-            } else {
-                response.statusCode = 302;
-                response.setHeader("Location", "/");
-                response.end();
-            }
+
+    else if (request.url == "/create" && request.method == "POST") {
+        const data = [];
+
+        request.on("data", (chunk) => {
+            data.push(chunk);
+        });
+        request.on("end", () => {
+            const result = Buffer.concat(data).toString();
+            const parsedData = result.split("=")[1];
+
+            fs.appendFile("blogs.txt", parsedData, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    response.statusCode = 302;
+                    response.setHeader("Location", "/");
+                    response.end();
+                }
+            });
         });
     }
-    else if (request.url == '/create') {
+
+    else if (request.url == "/create") {
         fs.readFile("create.html", (error, html) => {
             response.writeHead(200, { "Content-Type": "text/html" });
             response.write(html);
             response.end();
         });
     }
+
     else {
         fs.readFile("404.html", (error, html) => {
             response.writeHead(404, { "Content-Type": "text/html" });
@@ -44,4 +59,5 @@ var server = http.createServer((request, response) => {
 });
 
 server.listen(3000);
+
 console.log("node.js server at port 3000");
