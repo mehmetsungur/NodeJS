199c48a7b0519968175030a66253ef666b9cf7cc
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-16 17:32:35 +0300

    update

--- app.js
+++ app.js
@@ -4,14 +4,61 @@ var server = http.createServer((request, response) => {
     // console.log(request.url, request.method);
     // console.log(response.statusCode);
 
-    response.setHeader("Content-Type", "text/html");
-    response.statusCode = 200;
-    response.statusMessage = "OK";
+    // response.setHeader("Content-Type", "text/html");
+    // response.statusCode = 200;
+    // response.statusMessage = "OK";
 
-    response.write("<h1>Anasayfa</h1>");
-    response.write("<p>Urunler</p>");
+    // response.write("<h1>Anasayfa</h1>");
+    // response.write("<p>Urunler</p>");
 
-    response.end();
+    if (request.url == '/') {
+        response.writeHead(200, { "Content-Type": "text/html" });
+
+        response.write(`
+            <html>
+                <head>
+                    <title>Anasayfa</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>Anasayfa</h1>
+                </body>
+            </html>`);
+
+        response.end();
+    }
+    else if (request.url == '/blogs') {
+        response.writeHead(200, { "Content-Type": "text/html" });
+
+        response.write(`
+            <html>
+                <head>
+                    <title>Blog</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>Blog</h1>
+                </body>
+            </html>`);
+
+        response.end();
+    }
+    else {
+        response.writeHead(404, { "Content-Type": "text/html" });
+
+        response.write(`
+            <html>
+                <head>
+                    <title>404</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>Sayfa Bulunamadı</h1>
+                </body>
+            </html>`);
+
+        response.end();
+    }
 });
 
 
