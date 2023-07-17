9e034d4109a968c702f90bf6e391566d8906be7c
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-17 15:18:02 +0300

    update

--- blogapp/index.js
+++ blogapp/index.js
@@ -1,24 +1,18 @@
-const exp = require("constants");
 const express = require("express");
+
 const app = express();
+
 const path = require("path");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
 
-app.use("/libs", express.static("node_modules"));
 
-app.use("/blogs/:blogId", function (req, res) {
-    console.log(__dirname);
-    console.log(__filename);
-    res.sendFile(path.join(__dirname, "views/users", "blog-details.html"));
-});
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.use("/blogs", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/users", "blogs.html"));
-});
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
 
-app.use("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/users", "index.html"));
-});
-
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("listening on port 3000");
 });
---
+++ blogapp/routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const path = require("path");
+
+router.use("/blogs/:blogid", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/users","blog-details.html"));
+});
+
+router.use("/blogs", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/users","blogs.html"));
+});
+
+router.use("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/users","index.html"));
+});
+
+module.exports = router;
